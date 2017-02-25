b5e5c4120a366673e855434e196808478e776d5c
lkamrat <kamrat9@gmail.com>
2017-02-25 09:43:29 +0200

    Q1

--- HU_M01L06/Program.cs
+++ HU_M01L06/Program.cs
@@ -10,6 +10,21 @@ namespace HU_M01L06
     {
         static void Main(string[] args)
         {
+            // Q1 - Print user input numbers in reverse order 
+            int[] newArray = new int[5];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.WriteLine("Please enter a number");
+                newArray[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Reverse(newArray);
+            foreach (int value in newArray)
+            {
+                Console.WriteLine("\n" + value);
+            }
+
+
         }
     }
 }
